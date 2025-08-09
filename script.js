56eeac8204be194255da8c57a18c107ae9d25c4e
Nitin Mane <teraitaservices@gmail.com>
2025-08-09 18:58:23 +0530

    added

---
+++ script.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", () => {
+      const counters = document.querySelectorAll(".stat-number");
+      counters.forEach(counter => {
+        const updateCount = () => {
+          const target = +counter.getAttribute("data-target");
+          const count = +counter.innerText.replace(/[^0-9]/g, "");
+          const increment = target / 200;
+          if (count < target) {
+            counter.innerText = "+" + Math.ceil(count + increment).toLocaleString();
+            setTimeout(updateCount, 10);
+          } else {
+            counter.innerText = "+" + target.toLocaleString();
+          }
+        };
+        updateCount();
+      });
+    });
+
+//     function toggleMenu() {
+//     const navList = document.querySelector('nav ul');
+//     navList.classList.toggle('show');
+//   }
+
+function toggleMenu() {
+    const nav = document.getElementById('main-nav');
+    const toggleBtn = document.getElementById('menuToggle');
+
+    nav.classList.toggle('show');
+
+    // Toggle between ☰ and ✕
+    if (nav.classList.contains('show')) {
+      toggleBtn.style.display = 'none'; // Hide ☰
+    } else {
+      toggleBtn.style.display = 'block'; // Show ☰
+    }
+  }
+
+  let lastScrollTop = 0;
+  const threshold = 10; // pixels to consider as actual scroll
+  const searchBar = document.getElementById("searchBarWrapper");
+
+  window.addEventListener("scroll", function () {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (window.innerWidth <= 768) {
+      const scrollDiff = currentScroll - lastScrollTop;
+
+      if (Math.abs(scrollDiff) > threshold) {
+        if (scrollDiff > 0) {
+          // Scrolled down
+          searchBar.classList.add("hide-on-scroll");
+        } else {
+          // Scrolled up
+          searchBar.classList.remove("hide-on-scroll");
+        }
+        lastScrollTop = currentScroll;
+      }
+    }
+  });
